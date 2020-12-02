b57378aad24a0fcd6263a9d0d6f2ce71c8cbb67c
SakaRenko <985558237@qq.com>
2020-12-02 15:57:43 +0800

    practice 0 finished

--- lab3/kern/mm/default_pmm.c
+++ lab3/kern/mm/default_pmm.c
@@ -126,6 +126,7 @@ default_alloc_pages(size_t n) {
         return NULL;
     }
     struct Page *page = NULL;
+    //maintanence the order of lists
     list_entry_t *le = &free_list;
     while ((le = list_next(le)) != &free_list) {
         struct Page *p = le2page(le, page_link);
@@ -135,12 +136,12 @@ default_alloc_pages(size_t n) {
         }
     }
     if (page != NULL) {
-        list_del(&(page->page_link));
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
-            list_add(&free_list, &(p->page_link));
+            list_add(le, &(p->page_link));
     }
+        list_del(&(page->page_link));
         nr_free -= n;
         ClearPageProperty(page);
     }
@@ -159,8 +160,11 @@ default_free_pages(struct Page *base, size_t n) {
     base->property = n;
     SetPageProperty(base);
     list_entry_t *le = list_next(&free_list);
+    list_entry_t *sit = &free_list;
     while (le != &free_list) {
         p = le2page(le, page_link);
+        if(p + p->property < base)
+            sit = le;
         le = list_next(le);
         if (base + base->property == p) {
             base->property += p->property;
@@ -175,7 +179,7 @@ default_free_pages(struct Page *base, size_t n) {
         }
     }
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
+    list_add(sit, &(base->page_link));
 }
 
 static size_t
@@ -292,6 +296,7 @@ default_check(void) {
 
     le = &free_list;
     while ((le = list_next(le)) != &free_list) {
+        assert(le->next->prev == le && le->prev->next == le);
         struct Page *p = le2page(le, page_link);
         count --, total -= p->property;
     }
